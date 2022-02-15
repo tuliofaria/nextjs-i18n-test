db57409bc38c0620df3b6267e00c38095b476416
Tulio Faria <tuliofaria@gmail.com>
2022-02-15 16:43:34 -0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import type { GetServerSidePropsContext, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useTranslation } from "next-i18next";
 const Home: NextPage = () => {
+  const { t } = useTranslation("common");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,13 +15,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{t("h1")}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          <Link href="/" locale="en">
+            <a>English</a>
+          </Link>{" "}
+          <Link href="/" locale="es">
+            <a>Spanish</a>
+          </Link>
         </p>
 
         <div className={styles.grid}>
@@ -59,14 +63,26 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+};
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Link from "next/link";
+
+export async function getStaticProps({ locale }: GetServerSidePropsContext) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || "", ["common", "footer"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
 
-export default Home
+export default Home;
